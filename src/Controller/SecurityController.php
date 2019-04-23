58df9dc3b7a82501d96576df7ac250b32ef46492
mals44 <mals44@yahoo.fr>
2019-04-23 11:49:21 +0200

    formulaire d'inscription completement ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 
@@ -16,7 +17,7 @@ class SecurityController extends AbstractController
     /**
      * @Route("/inscription", name="security_registration")
      */
-    public function registration(Request $request, ObjectManager $manager) {
+    public function registration(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder) {
         
         $user = new User();
 
@@ -25,6 +26,8 @@ class SecurityController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
             $manager->persist($user);
             $manager->flush();
         }
